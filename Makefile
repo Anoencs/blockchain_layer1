e2e067c4fb1178846cb9af9805decfb2061440c4
Nguyễn Ngọc Thái An <57495997+Anoencs@users.noreply.github.com>
2023-04-01 20:14:14 +0700

    chap 1

---
+++ Makefile
@@ -0,0 +1,7 @@
+build: 
+	go build -o bin/blockchain_layer1
+run: build
+	./bin/blockchain_layer1
+test:
+	go test ./...
+	
---
+++ network/local_transport.go
@@ -0,0 +1,54 @@
+package network
+
+import (
+	"fmt"
+	"sync"
+)
+
+type LocalTransport struct {
+	addr      NetAddr
+	consumeCh chan RPC
+	peers     map[NetAddr]*LocalTransport
+	lock      sync.RWMutex
+}
+
+func NewLocalTransport(addr NetAddr) *LocalTransport {
+	return &LocalTransport{
+		addr:      addr,
+		consumeCh: make(chan RPC, 1024),
+		peers:     make(map[NetAddr]*LocalTransport),
+	}
+}
+func (tr *LocalTransport) Addr() NetAddr {
+	return tr.addr
+}
+
+func (tr *LocalTransport) Consume() <-chan RPC {
+	return tr.consumeCh
+}
+
+func (tr *LocalTransport) Connect(trb *LocalTransport) error {
+	tr.lock.Lock()
+	defer tr.lock.Unlock()
+
+	tr.peers[trb.Addr()] = trb
+	return nil
+}
+
+func (tr *LocalTransport) SendMessage(to NetAddr, payload []byte) error {
+	if tr.addr == to {
+		return fmt.Errorf("could not send message to yourself!!!")
+	}
+
+	peer, ok := tr.peers[to]
+	if !ok {
+		return fmt.Errorf("%s: could mpt send message to %s", tr.addr, to)
+	}
+
+	peer.consumeCh <- RPC{
+		From:    tr.addr,
+		Payload: payload,
+	}
+
+	return nil
+}
